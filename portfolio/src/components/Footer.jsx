d3b812c9022a5966c0b9539f745d93de697c6a30
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-18 11:31:42 -0500

    created footer

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import GithubLogo from '../Assets/github.svg'
+import LinkedInLogo from '../Assets/linkedin.svg'
+const Footer = () => {
+  return (
+    <div className='FooterContainer'>
+      <p className='FooterHeader'>Let's Connect:</p>
+      <a className='FooterLink' href="https://github.com/sramalho94">
+        <img src={GithubLogo} className='FooterIcon'/>
+      </a>
+      <a className='FooterLink'href="https://www.linkedin.com/in/stephan-ramalho/">
+      <img src={LinkedInLogo} className='FooterIcon'/>
+      </a>
+    </div>
+  )
+}
+
+export default Footer
