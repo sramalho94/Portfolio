48ed995748fad02c7bc64a57faa8df84812e74a8
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-18 11:33:01 -0500

    added home page

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Projects from '../components/Projects'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>Home
+      <Projects />
+    </div>
+    
   )
 }
 
