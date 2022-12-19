cfc7f7179289299130b0d528e624084c123eb4c1
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-19 12:29:37 -0500

    added route to App.js for projects component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Projects from '../components/Projects'
+import WordAnimation from '../components/WordAnimation'
 
 const Home = () => {
   return (
     <div className='home-container'>
-      <Projects />
+      <div className='home-text-container'>
+        <div className='home-text'>
+          <h1 className='home-title'>Stephan D Ramalho</h1>
+          <h2 className='home-sub-title'>Junior <br/>Software <br/> Engineer</h2>
+        </div>
+      <WordAnimation />
+      <div className='explore-container'>
+        
+          <a href='/projects'>
+          <h3 className='explore-text'> Explore Work</h3>
+          </a>
+      </div>
+      </div>
+      
     </div>
     
   )
