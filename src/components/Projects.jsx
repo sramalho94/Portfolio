d304bfb548042b6afcd194cb0aed7114b0a15aa1
Stephan D Ramalho <sramalho@fordham.edu>
2022-12-19 19:36:22 -0500

    added project descriptions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,6 +14,9 @@ const Projects = () => {
           <div className='project-image-container'>
             <img src="https://i.imgur.com/viiKyIb.png" className='project-image'/>
           </div>
+          <div className='project-description-container'>
+            <h3 className='project-description'> In this first project, I created a web browser version of the classic board game, Battleship. Using only vanilla JavaScript I created a fully functional game where the player goes against an AI. When either the player or the AI runs out of ships, the game is over</h3>
+          </div>
           <div className='project-link'>
             <a href='https://admiralstephanbattleship.surge.sh/'>Project Link</a>
           </div>
@@ -28,6 +31,9 @@ const Projects = () => {
           </div>
           <div className='project-image-container'>
             <img src="https://i.imgur.com/R1a9BlX.png" className='project-image'/>
+          </div>
+          <div className='project-description-container'>
+            <h3 className='project-description'> In this group hackathon project, our group created a theme parked themed web page. There are a number of themes and attractions to navigate through as you explore the page. The user also has the capability to add and delete attractions as well.</h3>
           </div>
           <div className='project-link'>
             <a href='https://corney-island2022.herokuapp.com/'>Project Link</a>
@@ -44,6 +50,10 @@ const Projects = () => {
           <div className='project-image-container'>
             <img src="https://i.imgur.com/bKO85LN.png" className='project-image'/>
           </div>
+          <div className='project-description-container'>
+            <h3 className='project-description'> The Faults In Our Stars is a hyper-personalized, social experience bringing astrology into the 22nd century. Our aim is to create social circles revolving around zodiac signs. This website will allow you to connect to people that are compatible to your zodiac sign and at the same time rate the users based on their personalities and sign compatibility; wether good or bad, create new social circles, that will improve you work and love life.
+            </h3>
+          </div>
           <div className='project-link'>
             <a href='https://starz-app.herokuapp.com/'>Project Link</a>
           </div>
@@ -58,6 +68,9 @@ const Projects = () => {
           </div>
           <div className='project-image-container'>
             <img src="https://i.imgur.com/KHzfbsY.png" className='project-image'/>
+          </div>
+          <div className='project-description-container'>
+            <h3 className='project-description'> In this final project for General Assembly's Software Engineering Immersive, I made a full stack web application that allows users to look up trainers in the base pokemon games and see their pokemon team. Suggestions are given on how to beat each pokemon based on that Pokemon's type. </h3>
           </div>
           <div className='project-link'>
             <a href='https://pokepro-frontend.herokuapp.com/'>Project Link</a>
